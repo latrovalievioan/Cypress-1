983bba32896d42ff968f098bba02437bd5a83270
Ioan Latrovaliev <latrovalievioan@gmail.com>
2021-02-03 12:38:31 +0200

    feat: implement login-form validation

---
+++ cypress/integration/login-form-validation.spec.js
@@ -0,0 +1,36 @@
+/// <reference types="cypress"/>
+describe("Validate Login Form", () => {
+  beforeEach(() => {
+    cy.visit("http://template3.booost.bg/");
+  });
+
+  it("Have visible login form", () => {
+    cy.get("[data-cy=login-form] > .form").should(
+      "have.css",
+      "visibility",
+      "visible"
+    );
+  });
+
+  it("Should have title - Account Login", () => {
+    cy.get("[data-cy=login-form] > .form-box-title").should(
+      "have.text",
+      "Account Login"
+    );
+  });
+
+  it("Should have two input fields", () => {
+    cy.get("[data-cy=login-form] > .form input").should(
+      "have.lengthOf.greaterThan",
+      1
+    );
+  });
+  it("Should have register button", () => {
+    cy.get(".register-switch").should("exist");
+  });
+  it("Register button should be visible after click", () => {
+    cy.get(".register-switch")
+      .click()
+      .should("have.css", "visibility", "visible");
+  });
+});
